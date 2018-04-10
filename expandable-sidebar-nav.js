4a85bd1b45003899ecd47965affd63c3c04b1d68
SeanDanielsDigital <34247055+SeanDanielsDigital@users.noreply.github.com>
2018-04-10 10:58:44 -0400

    Add files via upload

---
+++ expandable-sidebar-nav.js
@@ -0,0 +1,28 @@
+  if ($('.side-menu li ul .nolistHighlight')[0]){
+	  $(this).parent().css('display', 'block');
+	  $(this).parent().parent().children('a').addClass('active');
+  }
+
+  $('.side-menu li').on('click', 'a', function(e){
+	  
+    if ($(this).parent().children('ul').length){
+		e.preventDefault();
+		$(this).addClass('active');
+		$(this).parent().children('ul').slideDown();
+		setTimeout(function(){ 
+		  $.fn.matchHeight._update();
+		  $.fn.matchHeight._maintainScroll = true;
+		}, 1000);
+	}	
+	  	  
+  });
+
+  $('.side-menu li').on('click', 'a.active', function(e){
+    e.preventDefault();
+    $(this).removeClass('active');
+	$(this).parent().children('ul').slideUp();  
+    setTimeout(function(){ 
+      $.fn.matchHeight._update();
+      $.fn.matchHeight._maintainScroll = true;
+    }, 1000);
+  });
